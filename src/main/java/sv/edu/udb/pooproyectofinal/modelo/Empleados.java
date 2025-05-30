27b4f9709bb5f80d49a11101b96b1b0f90345f29
Artu23aa <arturoarias7102@gmail.com>
2025-05-29 22:33:27 -0600

    Update Empleados.java

--- src/main/java/sv/edu/udb/pooproyectofinal/modelo/Empleados.java
+++ src/main/java/sv/edu/udb/pooproyectofinal/modelo/Empleados.java
@@ -1,14 +1,9 @@
 package sv.edu.udb.pooproyectofinal.modelo;
 
-import java.sql.*;
-
 public class Empleados extends Persona {
-    private String carnet;
-    private String tipoContratacion;
-    
+    private String carnet, tipoContratacion;
     public Empleados() {}
 
-    // Getters y setters
     public String getCarnet() {
         return carnet;
     }
@@ -23,71 +18,5 @@ public class Empleados extends Persona {
 
     public void setTipoContratacion(String tipoContratacion) {
         this.tipoContratacion = tipoContratacion;
-    }
-
-    // Métodos para operaciones con la base de datos
-    public boolean insertar(Connection conn) throws SQLException {
-        String sql = "INSERT INTO empleado (carnet, dui, nombre, tipo_persona, telefono, email, direccion, tipo_contratacion, estado, creado_por) " +
-                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, this.carnet);
-            pstmt.setString(2, this.getDui());
-            pstmt.setString(3, this.getNombre());
-            pstmt.setString(4, this.getTipoPersona());
-            pstmt.setString(5, this.getTelefono());
-            pstmt.setString(6, this.getEmail());
-            pstmt.setString(7, this.getDireccion());
-            pstmt.setString(8, this.tipoContratacion);
-            pstmt.setBoolean(9, this.isEstado());
-            pstmt.setString(10, this.getCreadoPor());
-            
-            return pstmt.executeUpdate() > 0;
-        }
-    }
-
-    public boolean actualizar(Connection conn) throws SQLException {
-        String sql = "UPDATE empleado SET dui=?, nombre=?, tipo_persona=?, telefono=?, email=?, " +
-                     "direccion=?, tipo_contratacion=?, estado=?, actualizado_por=?, fecha_actualizacion=CURRENT_TIMESTAMP " +
-                     "WHERE carnet=?";
-        
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, this.getDui());
-            pstmt.setString(2, this.getNombre());
-            pstmt.setString(3, this.getTipoPersona());
-            pstmt.setString(4, this.getTelefono());
-            pstmt.setString(5, this.getEmail());
-            pstmt.setString(6, this.getDireccion());
-            pstmt.setString(7, this.tipoContratacion);
-            pstmt.setBoolean(8, this.isEstado());
-            pstmt.setString(9, this.getActualizadoPor());
-            pstmt.setString(10, this.carnet);
-            
-            return pstmt.executeUpdate() > 0;
-        }
-    }
-
-    public boolean cargarPorId(Connection conn, String carnet) throws SQLException {
-        String sql = "SELECT * FROM empleado WHERE carnet = ?";
-        
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, carnet);
-            ResultSet rs = pstmt.executeQuery();
-            
-            if (rs.next()) {
-                this.carnet = rs.getString("carnet");
-                this.setDui(rs.getString("dui"));
-                this.setNombre(rs.getString("nombre"));
-                this.setTipoPersona(rs.getString("tipo_persona"));
-                this.setTelefono(rs.getString("telefono"));
-                this.setEmail(rs.getString("email"));
-                this.setDireccion(rs.getString("direccion"));
-                this.tipoContratacion = rs.getString("tipo_contratacion"));
-                this.setEstado(rs.getBoolean("estado"));
-                this.setCreadoPor(rs.getString("creado_por"));
-                return true;
-            }
-            return false;
-        }
     }
 }
